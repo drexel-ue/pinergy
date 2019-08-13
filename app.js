6f642958a79fe1cd2e3bc06870db8e495f7e26e4
ikesh pack <packappsforyou@gmail.com>
2019-08-12 20:16:58 -0400

    Configured app.js

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
+const mongoose = require("mongoose");
+// This creates a new Express server.
 const express = require("express");
 const app = express();
+const db = require("./config/keys").mongoURI;
+// Imports created routes.
 const users = require("./routes/api/users");
+// Allows us to parse the json sent to the front end.
+const bodyParser = require("body-parser");
+// Verifies incoming request tokens to project routes.
+const passport = require("passport");
+require("./config/passport")(passport);
+const path = require("path");
 
-app.get("/", (req, res) => res.send("Hello World"));
+// Sets up connection to MondoDB.
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
-app.use("/api/users", users);
-
+// Tells the server which potrt to run on.
 const port = process.env.PORT || 5000;
 
+// Sets up a basic route so that we can render some information on our page.
+// app.get("/", (req, res) => res.send("Sistine Camels"));
+
+// Sets passport as the bouncer.
+app.use(passport.initialize());
+
+// Parse application/x-www-form-urlencoded.
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json.
+app.use(bodyParser.json());
+
+// We must tell Express to use imported routes.
+app.use("/api/users", users);
+
+// Tells Express to start a socket and listen for connections on the path.
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// Tells our server to load the static build folder in production.
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
