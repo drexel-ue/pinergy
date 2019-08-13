fd584facc9aa9e33ef07c5b77e2d0780535bddc7
ikesh pack <packappsforyou@gmail.com>
2019-08-13 11:28:30 -0400

    Added basic modal

---
+++ frontend/src/components/modal/modal_container.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { closeModal } from "../actions/modal_actions";
+import { connect } from "react-redux";
+
+function Modal({ modal, closeModal }) {
+  if (!modal) {
+    return null;
+  }
+  let component;
+  switch (modal.type) {
+    case "photoShow":
+      component = <PostShowContainer data={modal.data} />;
+      break;
+    case "commentError":
+      component = <CommentErrorModal />;
+      break;
+    default:
+      return null;
+  }
+  return (
+    <div className="modal-background" onClick={closeModal}>
+      <div className="modal-child" onClick={e => e.stopPropagation()}>
+        {component}
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    modal: state.ui.modal
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Modal);
