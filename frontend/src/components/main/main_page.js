f2b207dd02f407d39e6ab1426ed2dcca12241669
ikesh pack <packappsforyou@gmail.com>
2019-08-13 10:22:31 -0400

    Added basic main page component

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>A Pinterest Clone</h1>
+        <footer>Copyright &copy; 2019 Pinergy</footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
