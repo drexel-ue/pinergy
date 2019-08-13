e324e6e440538ab264f70df6ac51571a4c01746c
ikesh pack <packappsforyou@gmail.com>
2019-08-13 11:49:43 -0400

    Added modal to app

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,9 +6,11 @@ import NavBarContainer from "./nav/navbar_container";
 import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
+import ModalContainer from "./modal/modal_container";
 
 const App = () => (
   <div>
+    <ModalContainer />
     <NavBarContainer />
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
