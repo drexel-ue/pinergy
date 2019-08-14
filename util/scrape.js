496f8058cadc02c4dfb7218ddbb8259ddb48ccbd
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-08-14 13:54:05 -0700

    Fix Scraper

--- util/scrape.js
+++ util/scrape.js
@@ -1,27 +1,22 @@
 const puppeteer = require("puppeteer");
 
 exports.scrape = async keyWords => {
-  // debugger;
   const browser = await puppeteer.launch();
-  // debugger;
 
   const page = await browser.newPage();
-  // debugger;
 
   page.goto("https://unsplash.com");
-  // debugger;
   let data;
 
   if (!keyWords) {
-    // debugger;
     await page.waitForSelector("img", { visible: true });
 
     data = await page.evaluate(() => {
       const images = document.querySelectorAll("img");
-      // debugger;
-      return [...images].map(image => image.src);
+      const initUrls = [...images].map(image => image.src);
+
+      return initUrls.filter(url => url.includes("http"))
     });
-    debugger
   } else {
     await page.type("[name=searchKeyword]", keyWords.join(" "));
     await page.click("[type=sumbit]");
@@ -36,7 +31,6 @@ exports.scrape = async keyWords => {
 
   browser.close();
 
-  debugger;
 
   return data;
 };
