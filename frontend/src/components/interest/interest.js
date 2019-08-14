3cc637fcb6fd84ff838e06bbf3137350a02538c4
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-08-14 14:33:16 -0700

    Give interest files default imgUrl

--- frontend/src/components/interest/interest.js
+++ frontend/src/components/interest/interest.js
@@ -1,31 +1,30 @@
 import React from "react";
 
-
-
 export default class Interest extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgUrl: "https://cdn2.iconfinder.com/data/icons/bold-ui/100/questionmark-2-512.png"
-    }
+      imgUrl:
+        "https://cdn2.iconfinder.com/data/icons/bold-ui/100/questionmark-2-512.png"
+    };
   }
   componentDidMount() {
-    this.props.scrapeImage(this.props.interest)
-      .then(res => {
-        debugger
-        this.setState({})
-      })
+    this.props.scrapeImage(this.props.interest).then(res => {
+      debugger;
+      this.setState({});
+    });
   }
-  
+
   render() {
+    const style = {
+      background: `url("${this.state.imgUrl}")`,
+      backgroundSize: "contain",
+      backgroundColor: "grey"
+    };
     return (
-      <div
-        id={this.props.interest}
-        className="interest"
-      > <img className="iimg" src={this.state.imgUrl}/>
+      <div id={this.props.interest} style={style} className="interest">
         <div className="interest_text">{this.props.interest}</div>
       </div>
     );
   }
 }
-
