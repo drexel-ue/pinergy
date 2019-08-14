2470c625f865b3953223cf65e6b6722eea21e2e3
ikesh pack <packappsforyou@gmail.com>
2019-08-14 00:25:47 -0400

    Clears user update errors

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -4,15 +4,20 @@ import { receiveCurrentUser } from "./session_actions";
 export const RECEIVE_USER_UPDATE_ERRORS = "RECEIVE_USER_UPDATE_ERRORS";
 export const USER_UPDATED = "USER_UPDATED";
 
-export const receiveErrors = errors => ({
+const receiveErrors = errors => ({
   type: RECEIVE_USER_UPDATE_ERRORS,
   errors
+});
+
+const userUpdated = () => ({
+  type: USER_UPDATED
 });
 
 export const updateUser = (userData, id) => dispatch =>
   ApiUtil.updateUser(userData, id)
     .then(response => {
       dispatch(receiveCurrentUser(response.data));
+      dispatch(userUpdated());
     })
     .catch(err => {
       dispatch(receiveErrors(err.response.data));
