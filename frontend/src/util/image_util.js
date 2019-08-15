0b2765773fd50d247b26d72f2ba1ffcca0d70f8e
ikesh pack <packappsforyou@gmail.com>
2019-08-14 20:58:22 -0400

    Install puppeteer

--- frontend/src/util/image_util.js
+++ frontend/src/util/image_util.js
@@ -1,7 +1,8 @@
 import Axios from "axios";
 
 export const scrapeImage = interest => {
-  return Axios.post('/api/pins/query', interest)
-    .then(res => { debugger })
-  // .catch(res => {debugger})  
-}
+  return Axios.post("/api/pins/query", interest).then(res => {
+    debugger;
+  });
+  // .catch(res => {debugger})
+};
