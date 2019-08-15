8f13c8f883c20d6de5b694bf742921200fdf605b
ikesh pack <packappsforyou@gmail.com>
2019-08-14 23:24:15 -0400

    Adds action to fetch and dispatch image urls

--- frontend/src/actions/scrape_actions.js
+++ frontend/src/actions/scrape_actions.js
@@ -1,10 +1,17 @@
 import Axios from "axios";
-import interests from "";
+import interests from "../util/interest_util";
+
+export const RECEIVE_INTERESTS = "RECEIVE_INTERESTS";
+
+const receiveInterests = interests => ({
+  type: RECEIVE_INTERESTS,
+  interests
+});
 
 export const scrapeInterests = () => dispatch => {
   return Axios.post("/api/pins/query", interest)
-    .then(res => {
-      debugger;
+    .then(({ data }) => {
+      dispatch(receiveInterests(data));
     })
     .catch(res => {
       debugger;
