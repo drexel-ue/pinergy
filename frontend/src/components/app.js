5e509c756985e4f01e00f5853576974bd7f8e641
ikesh pack <packappsforyou@gmail.com>
2019-08-16 14:29:06 -0400

    Fixes profile head container reference

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,28 +6,43 @@ import NavBarContainer from "./nav/navbar_container";
 import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
 import ModalContainer from "./modal/modal";
-import ProfileHeadContainer from "./profile/profile_head_container"
+import ProfileHeadContainer from "./profile/profile_head_container";
 
 class App extends React.Component {
-
   render() {
-  return (
-    <div>
-      <ModalContainer />
-      <NavBarContainer />
-      <Switch>
-        <AuthRoute exact path="/" component={MainPage} />
-        <ProtectedRoute path="/profile" component={ProfileHeadContainer} />
-        <ProtectedRoute exact path="/profile/boards" component={ProfileContainer} />
-        <ProtectedRoute exact path="/profile/pins" component={ProfileContainer} />
-        <ProtectedRoute exact path="/profile/tries" component={ProfileContainer} />
-        <ProtectedRoute exact path="/profile/topics" component={ProfileContainer} />
-        {/* <AuthRoute exact path="/login" component={LoginFormContainer} /> */}
-        {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
-      </Switch>
-    </div>
-  )
+    return (
+      <div>
+        <ModalContainer />
+        <NavBarContainer />
+        <Switch>
+          <AuthRoute exact path="/" component={MainPage} />
+          <ProtectedRoute path="/profile" component={ProfileHeadContainer} />
+          <ProtectedRoute
+            exact
+            path="/profile/boards"
+            component={ProfileHeadContainer}
+          />
+          <ProtectedRoute
+            exact
+            path="/profile/pins"
+            component={ProfileHeadContainer}
+          />
+          <ProtectedRoute
+            exact
+            path="/profile/tries"
+            component={ProfileHeadContainer}
+          />
+          <ProtectedRoute
+            exact
+            path="/profile/topics"
+            component={ProfileHeadContainer}
+          />
+          {/* <AuthRoute exact path="/login" component={LoginFormContainer} /> */}
+          {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
+        </Switch>
+      </div>
+    );
   }
-};
+}
 
 export default App;
