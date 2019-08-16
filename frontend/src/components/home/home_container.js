8789906ed9d56d88abfbdb51d1b2d97304fd9174
ikesh pack <packappsforyou@gmail.com>
2019-08-16 17:23:29 -0400

    Sets up basic home container

---
+++ frontend/src/components/home/home_container.js
@@ -0,0 +1,15 @@
+import { connect } from "react-redux";
+import Home from "./home";
+import { fetchPins } from "../../actions/pin_actions";
+
+const msp = ({ entities }) => ({
+  pins: entities.pins
+});
+const mdp = dispatch => ({
+  getAll: () => dispatch(fetchPins([]))
+});
+
+export default connect(
+  msp,
+  mdp
+);
