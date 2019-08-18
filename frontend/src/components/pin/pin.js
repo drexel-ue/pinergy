6e7194304d18eb21e7f71a2ac8722e3a6717f5ed
Clayton <claytonajones839@gmail.com>
2019-08-18 14:43:41 -0400

    Switch open pin modal only open on image click

--- frontend/src/components/pin/pin.js
+++ frontend/src/components/pin/pin.js
@@ -4,6 +4,7 @@ import "../home/home.css";
 export default class Pin extends React.Component {
   constructor(props) {
     super(props);
+
     this.showPin = this.showPin.bind(this);
     this.parseDestinationLink = this.parseDestinationLink.bind(this)
   }
@@ -22,8 +23,6 @@ export default class Pin extends React.Component {
     return hostDomain;
   }
 
-  render() {
-
   showPin(event) {
     event.preventDefault();
     this.props.showPin(this.props.pin._id);
@@ -31,8 +30,10 @@ export default class Pin extends React.Component {
 
   render() {
     return (
-      <div className="home-pin-wrap" onClick={this.showPin}>
-        <img className="home-pin-img" src={this.props.pin.url} />
+      <div className="home-pin-wrap" >
+        <img className="home-pin-img" 
+          onClick={this.showPin} 
+          src={this.props.pin.url} />
         <div className="pin-button home-save">
           <i class='fas fa-thumbtack'></i>
           &nbsp;&nbsp;Save
