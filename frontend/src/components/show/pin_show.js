681366ba3832fd589ef55ced6421d023332b3d9c
ikesh pack <packappsforyou@gmail.com>
2019-08-18 11:29:51 -0400

    Prep for handoff

--- frontend/src/components/show/pin_show.js
+++ frontend/src/components/show/pin_show.js
@@ -3,9 +3,19 @@ import "./pin_show.css";
 
 export default class PinShow extends React.Component {
   render() {
+    debugger;
     return (
       <div className="pin_show">
-        <img src={this.props.pin.url} alt={this.props.pin.title} />
+        <div className="pin_show_image_wapper">
+          <div className="pin_show_image_overlay">
+            {this.props.pin.destinationLink}
+          </div>
+          <img
+            className="pin_show_image"
+            src={this.props.pin.url}
+            alt={this.props.pin.title}
+          />
+        </div>
       </div>
     );
   }
