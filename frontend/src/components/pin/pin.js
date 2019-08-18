ee163631a60b2e39e2fc95eeb027ca9523e4e819
Clayton <claytonajones839@gmail.com>
2019-08-17 20:40:19 -0400

    Make buttons show only when pin is hovered

--- frontend/src/components/pin/pin.js
+++ frontend/src/components/pin/pin.js
@@ -6,9 +6,9 @@ export default class Pin extends React.Component {
 
     return (
       <div className="home-pin-wrap">
-        <div className="home-pin-save"> Save </div>
         <img className="home-pin-img" src={this.props.pin.url} />
-        <div className="home-pin-link"> www.test.com </div>
+        <div className="pin-button home-save"> Savasdfsdfse </div>
+        <div className="pin-button home-link"> www.test.com </div>
       </div>
     );
   }
