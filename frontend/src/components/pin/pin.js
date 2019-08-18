72df3e690ae87dd6dc0592b721c3c6671915df8d
Clayton <claytonajones839@gmail.com>
2019-08-17 20:52:04 -0400

    Add pin icon to save button

--- frontend/src/components/pin/pin.js
+++ frontend/src/components/pin/pin.js
@@ -7,7 +7,10 @@ export default class Pin extends React.Component {
     return (
       <div className="home-pin-wrap">
         <img className="home-pin-img" src={this.props.pin.url} />
-        <div className="pin-button home-save"> Savasdfsdfse </div>
+        <div className="pin-button home-save"> 
+          <i class='fas fa-thumbtack'></i>
+          Save
+        </div>
         <div className="pin-button home-link"> www.test.com </div>
       </div>
     );
