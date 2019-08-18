8d3a5e0dd67af78a1922ff6073729190cbb1b94f
Clayton <claytonajones839@gmail.com>
2019-08-17 23:47:06 -0400

    Add styling for pin buttons, route to pin url

--- frontend/src/components/pin/pin.js
+++ frontend/src/components/pin/pin.js
@@ -2,16 +2,48 @@ import React from "react";
 import "../home/home.css";
 
 export default class Pin extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.parseDestinationLink = this.parseDestinationLink.bind(this)
+  }
+
+  parseDestinationLink() {
+    let hostDomain;
+    let { destinationLink } = this.props.pin
+    // debugger;
+    if (destinationLink.indexOf("//") > -1) {
+      hostDomain = destinationLink.split('/')[2];
+    }
+    else {
+      hostDomain = destinationLink.split('/')[0];
+    }
+    hostDomain = hostDomain.split(':')[0];
+    hostDomain = hostDomain.split('?')[0];
+    // debugger;
+    return hostDomain;
+  }
+
   render() {
 
     return (
       <div className="home-pin-wrap">
         <img className="home-pin-img" src={this.props.pin.url} />
-        <div className="pin-button home-save"> 
+        <div className="pin-button home-save">
           <i class='fas fa-thumbtack'></i>
-          Save
+          &nbsp;&nbsp;Save
         </div>
-        <div className="pin-button home-link"> www.test.com </div>
+        <a 
+          href={this.props.pin.url}
+          target="_blank"
+          className="pin-button home-link"
+        >
+          <i class='fas fa-arrow-right'></i>
+          &nbsp;&nbsp;
+          <div className="pin-destination-link">
+            {this.parseDestinationLink()}
+          </div>
+        </a>
       </div>
     );
   }
