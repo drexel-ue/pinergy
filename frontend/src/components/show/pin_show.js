07b2f14bbc78abb618edd275f807eed172babb69
ikesh pack <packappsforyou@gmail.com>
2019-08-17 20:04:43 -0400

    Adds basic pin show

---
+++ frontend/src/components/show/pin_show.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./pin_show.css";
+
+export default class PinShow extends React.Component {
+  render() {
+    return <div className="pin_show">Pin Show</div>;
+  }
+}
