e994d5dc0fa73e9667ad0cfd6d69595fa32e82b0
ikesh pack <packappsforyou@gmail.com>
2019-08-19 10:50:11 -0400

    Adds case handling

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,18 +1,23 @@
-import {
-  RECEIVE_USER
-
-} from '../actions/user_actions'
-import {merge} from 'lodash'
-
+import { RECEIVE_USER, RECEIVE_USERS } from "../actions/user_actions";
+import { merge } from "lodash";
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_USER:
       return merge({}, state, { [action.user._id]: action.user });
+    case RECEIVE_USERS:
+      return merge(
+        {},
+        state,
+        action.users.reduce((pojo, pin) => {
+          pojo[pin._id] = pin;
+          return pojo;
+        }, {})
+      );
     default:
       return state;
   }
 };
 
-export default usersReducer
+export default usersReducer;
