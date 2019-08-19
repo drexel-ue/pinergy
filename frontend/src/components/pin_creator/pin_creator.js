ec85bd7c91d97247e775d1cb3fc27d989a21f98b
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-08-19 09:47:25 -0700

    Work on inputform"

--- frontend/src/components/pin_creator/pin_creator.js
+++ frontend/src/components/pin_creator/pin_creator.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { AnchorButton, Intent} from "@blueprintjs/core"
+import { AnchorButton, Intent, ProgressBar } from "@blueprintjs/core"
+import lodash from 'lodash'
+import { Icon } from 'react-icons-kit'
+import { remove } from 'react-icons-kit/fa/remove'
 export default class PinCreator extends React.Component {
   constructor(props) {
     super(props);
@@ -9,9 +12,10 @@ export default class PinCreator extends React.Component {
       board: "",
       destination_link: "",
       showDropDown: false,
-      inputUrl: false
+      inputUrl: false,
+      loadedFile =[]
     };
-    this.fileLoad = this.fileLoad.bind(this)
+    this.onFileLoad = this.FileLoad.bind(this)
   }
   componentDidMount() {
     this.props.fetchUser(this.props.id);
@@ -31,7 +35,54 @@ export default class PinCreator extends React.Component {
 
   }
   onFileLoad(e) {
+    const file = e.currentTarge.files[0]
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+      console.log("IMAGE LOADED: ", fileReader.Result)
+      const file = {
+        name: file.name,
+        size: file.size,
+        type: file.type,
+        data: fileReader.result
+      }
+      this.addLoadedFile(file)
+    }
+
+    fileReader.onabort = () => {
+      alert("Reading Aborted!");
+    }
+
+    fileReader.onerror = () => {
+      alert("Reading Error!")
+    }
     
+    fileReader.readAsDataURL(file)
+  }
+  addLoadedFile(file) {
+    // this.setState((prevState) => ({ loadedFiles: [...prevState.loadedFiles, file]}))
+    // above code will be used to add multiple files to the loading dock
+    this.setState((prevState) => ({ loadedFiles: [...prevState.loadedFiles, file] }))
+    
+  } // splat operator is used to concat new file and old file togeter
+
+  removeLoadedFile(file) { //onlyneeded if we're uploading images, left infor educational purpose
+    //remove file from the state
+    this.setState((prevState) => {
+      let loadedFiles = prevState.loadedFiles;
+      let newLoadedFiles = lodash.filert(loadedFiles, (ldFiles) => {
+        return ldFiles != file; //takes preloaded state and only returns ldFile that equals to file passedin
+      })
+      return { loadedFiles: newLoadedFiles }
+    })
+  }
+
+  removeAllLoadedFile() {
+    this.setState({ loadedFiles: [] })
+  }
+
+  turnOffInputUrl() {
+    e.preventDefault();
+    this.setState({ inputUrl: false});
   }
 
 
@@ -41,33 +92,57 @@ export default class PinCreator extends React.Component {
         <input type="text" className="urlinp" />
       </div>
     ) : (
-        <div>
-          <div className="subhdr">Drag and image</div>
-          <div className="fileuplder">
-            <input
-              type="file"
-              className="filebrserip"
-              onDrag={}
-              onDrop={}
-              onChange={} />
-          </div>
-          <div className="hlpertxt">Drag and Drop image here</div>
-          <div className="filebrowsbutton">
-            <AnchorButton
-              text="Browse"
-              intent={Intent.PRIMARY}
-              minimal={true}
-              onClick={() => this.fileInput.click()} />
-      </div>
-      </div>
+        <div className="beforeurlbtn" onClick={this.toggleInputUrl}>Save from site</div>
     );
   }
   render() {
+    const { loadedFile } = this.state;
     const user = this.props.currentUser
     return (
       <div className="">
 
         <form>
+          <div className="fileuplder">
+            <div className="subhdr">Drag and image</div>
+            <div className="fileuplder">
+              <input
+                type="file"
+                className="filebrserip"
+                onDrag={this.onDragHandler}
+                onDrop={this.onFileLoad}
+                onChange={this.onFileLoad} />
+            </div>
+            <div className="filepreview">
+              {loadedFile.map((file, idx) => {
+                return <div clasName='ldedimg' key={idx}>
+                  <img src={file.data} />
+                    <div className="ldingcontainer">
+                      <span className="prgrssbar">
+                        {file.isUploading && <ProgressBar />}
+                      </span>
+                      <span className="rmvbtn">
+                      <Icon
+                        icon={remove}
+                        className='rmvicon'
+                        onClick={this.removeAllLoadedFile}
+                      />
+                      </span>
+                    </div>
+                  </div>
+              })}
+            </div>
+            <div className="hlpertxt">Drag and Drop image here</div>
+            <div className="filebrowsbutton">
+              <AnchorButton
+                text="Browse"
+                intent={Intent.PRIMARY}
+                minimal={true}
+                onClick={() => this.fileInput.click()} />
+            </div>
+          </div>
+          <div className="urlform">
+
+          </div>
           <input type="text" className="ttlipbx" />
           <div>{user.username}</div>
           <img src={user.profilePhotoUrl} className="prfprfpho" />
