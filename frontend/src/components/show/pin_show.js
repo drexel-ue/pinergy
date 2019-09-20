58162bb0e037fa834b6db16346a5a56240485e58
ikesh pack <packappsforyou@gmail.com>
2019-09-20 16:26:30 -0400

    Change cursor

--- frontend/src/components/show/pin_show.js
+++ frontend/src/components/show/pin_show.js
@@ -5,7 +5,7 @@ export default class PinShow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showDropDown: false,
+      showDropDown: false
     };
 
     this.togglePinDrop = this.togglePinDrop.bind(this);
@@ -15,11 +15,11 @@ export default class PinShow extends React.Component {
 
   componentDidMount() {
     this.props.fetchPin(this.props.match.params.id);
-    window.addEventListener("click", this.toggleOffPinDrop)
+    window.addEventListener("click", this.toggleOffPinDrop);
   }
 
   componentWillUnmount() {
-    window.removeEventListener("click", this.toggleOffPinDrop)
+    window.removeEventListener("click", this.toggleOffPinDrop);
   }
 
   parseDestinationLink() {
@@ -57,51 +57,102 @@ export default class PinShow extends React.Component {
         </div>
       </div>
     ) : (
-        <div className="" />
-      );
+      <div className="" />
+    );
+  }
+
+  renderSaveBtn() {
+    return this.state.showDropDown ? (
+      <div className="hide-div" />
+    ) : (
+      <div className="board-save-btn" onClick={this.handleSubmit}>
+        Save
+      </div>
+    );
+  }
+
+  renderBoardMenu() {
+    return this.state.showDropDown ? (
+      <div className="board-drop-down">
+        {this.props.boards.map(ele => {
+          return (
+            <div
+              className="board-drop-item"
+              onClick={this.handleBoard}
+              value={ele._id}
+              name={ele.title}
+            >
+              <div>{ele.title}</div>
+            </div>
+          );
+        })}
+      </div>
+    ) : (
+      //
+      <div className="hide-div" />
+    );
+  }
+
+  handleBoard(e) {
+    e.preventDefault();
+    this.setState({
+      boardName: e.currentTarget.textContent,
+      boardId: e.currentTarget.attributes.value.value
+    });
   }
 
   render() {
+    const { boardName } = this.state;
     return this.props.pin ? (
       <div className="pin-show-outer">
         <div className="pin_show_image_wrapper">
           <div className="pin_stick_bar">
-            <div className="pin_menu"
-              onClick={this.togglePinDrop}>
+            <div className="pin_menu" onClick={this.togglePinDrop}>
               <i className="fas fa-ellipsis-h ell-show"></i>
               {this.pinDropDown()}
             </div>
             <div className="stick-bar-right">
               <div className="pin-show-share">
-                <i class='fas fa-share-alt'></i>
+                <i class="fas fa-share-alt"></i>
                 &nbsp;&nbsp;Share
               </div>
-              <div className="pin-show-save">
-                <i class="fas fa-thumbtack"></i>
-                &nbsp;&nbsp;Save
+              <div className="pin-create-board-dropdown">
+                <div
+                  className="board-select-text"
+                  onClick={this.toggleDropDown}
+                >
+                  {boardName ? boardName : <p>Select</p>}
+                </div>
+                <div>
+                  <i className="fas fa-chevron-down board-down" />
+                </div>
+                {this.renderSaveBtn()}
               </div>
+              {this.renderBoardMenu()}
             </div>
           </div>
           <div className="pin-show-bottom">
-          <div className="pin-show-left">
-            <a href={this.props.pin.url} target="_blank" className="pin-atag">
-              <img
-                className="pin-show-image"
-                src={this.props.pin.url}
-                alt={this.props.pin.title}
-              />
-              <div className="pin-show-link" onClick={this.openLink}>
-                <i className="fas fa-arrow-right"></i>
-                &nbsp;{this.parseDestinationLink()}
-              </div>
-            </a>
-          </div>
-          <div className="pin-show-right">
-            <div className="pin-show-info">
-              <div className="pin-show-title">{this.props.pin.title}</div>
-              <div className="pin-show-desc">{this.props.pin.description}</div>
+            <div className="pin-show-left">
+              <a href={this.props.pin.url} target="_blank" className="pin-atag">
+                <img
+                  className="pin-show-image"
+                  src={this.props.pin.url}
+                  alt={this.props.pin.title}
+                />
+                <div className="pin-show-link" onClick={this.openLink}>
+                  <i className="fas fa-arrow-right"></i>
+                  &nbsp;{this.parseDestinationLink()}
+                </div>
+              </a>
             </div>
-          </div>
+            <div className="pin-show-right">
+              <div className="pin-show-info">
+                <div className="pin-show-title">{this.props.pin.title}</div>
+                <div className="pin-show-desc">
+                  {this.props.pin.description}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
