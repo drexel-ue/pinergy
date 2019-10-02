b9449564779428988814a0fbfbf0fbb590fe93dc
ikesh pack <packappsforyou@gmail.com>
2019-10-02 19:04:26 -0400

    Center loader

--- frontend/src/components/loader/loader.js
+++ frontend/src/components/loader/loader.js
@@ -3,11 +3,9 @@ import "./loader.css";
 
 function Loader() {
   return (
-    <div className="SuperLoader">
-      <div className="loader center">
+   
         <i className="fa fa-cog fa-spin fa-10x" />
-      </div>
-    </div>
+
   );
 }
 
