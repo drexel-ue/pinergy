f92b42734a01de4ab2bd5be234f5d482377c3e55
Clayton <claytonajones839@gmail.com>
2019-10-03 17:22:14 -0400

    add rel noopener to _blank links

--- frontend/src/components/static_pages/project_details.js
+++ frontend/src/components/static_pages/project_details.js
@@ -16,7 +16,7 @@ export default class RequestFeature extends React.Component {
                 </div>
                 <div className="pd-right">
                     <div className="pd-desc">
-                        Pinergy is a web-application modeled after the popular social media website, <a className="pin-a-link" target="_blank" href="https://www.pinterest.com/"> Pinterest. </a> Pinergy was built using the MERN stack over the course of 2 weeks. This application utilized web-scraping and AWS S3 to obtain and store links from URLs. Try creating a new Pin with your own image, or enter a URL and selected from a list of images found on that page.
+                        Pinergy is a web-application modeled after the popular social media website, <a rel="noopener noreferrer" className="pin-a-link" target="_blank" href="https://www.pinterest.com/"> Pinterest. </a> Pinergy was built using the MERN stack over the course of 2 weeks. This application utilized web-scraping and AWS S3 to obtain and store links from URLs. Try creating a new Pin with your own image, or enter a URL and selected from a list of images found on that page.
                     </div>
                     <div className="pd-cont">
                         <div className="contributors">
@@ -26,24 +26,28 @@ export default class RequestFeature extends React.Component {
                             <a 
                                 className="team-member"
                                 target="_blank"
+                                rel="noopener noreferrer"
                                 href="https://github.com/Anshuj11">
                                 Anshu Jain
                             </a>
                             <a
                                 className="team-member"
                                 target="_blank"
+                                rel="noopener noreferrer"
                                 href="https://github.com/ClaytonJones839">
                                 Clayton Jones
                             </a>
                             <a
                                 className="team-member"
                                 target="_blank"
+                                rel="noopener noreferrer"
                                 href="https://github.com/drexel-ue">
                                 IKesh Pack
                             </a>
                             <a
                                 className="team-member"
                                 target="_blank"
+                                rel="noopener noreferrer"
                                 href="https://github.com/paulkwchoi92">
                                 Paul Kilwoung Choi
                             </a>
