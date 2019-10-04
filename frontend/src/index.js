34bab1f010573842a2a40c5c9752620d8b3e4143
Clayton <claytonajones839@gmail.com>
2019-10-04 18:07:29 -0400

    Remove sections from index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
       entities: {
         users: {},
         boards: {},
-        sections: {},
         pins: {}
       },
       session: { isAuthenticated: true, user: decodedUser },
